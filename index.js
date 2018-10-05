1c7cad6bf05fd712b059057a2542118557ce4944
Spokeek <spokeek@gmail.com>
2018-10-05 11:10:56 +0200

    added web server

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("coucou"); 
+const express = require('express')
+const app = express()
+
+const PORT = 3000
+
+app.get('/envoi-fichier', function (req, res) {
+  res.send("Page d'envoi de fichier")
+})
+
+app.listen(PORT, function () {
+  console.log(`NodeJS Version Perf started on port ${PORT}!`)
+})
