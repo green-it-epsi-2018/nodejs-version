3ee0d61b961b3ed525ccf521943dbfee8f6fa060
Tshili <Tshili@MacBook-Pro-de-Tshili.local>
2018-10-15 14:38:58 +0200

    clean code

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
 const express = require("express");
-const fileUpload = require("express-fileupload");
 var profile = require('./profile');
 
-
 const app = express();
-app.use(fileUpload());
-app.use('/upload', profile);
-
-
 const PORT = 3000;
 
-app.get("/envoi-fichier", function (req, res) {
-    res.send("Page d'envoi de fichier");
-});
+app.use('/', profile);
+
 
 app.listen(PORT, function () {
     console.log(`NodeJS Version Perf started on port ${PORT}!`);
--- profile.js
+++ profile.js
@@ -4,21 +4,28 @@ var multer = require("multer");
 
 // where to store file
 const multerConf = {
-  storage: multer.diskStorage({
-    destination: function(req, file, cb) {
-      cb(null, "./upload-files");
-    },
-    filename: function(req, file, cb) {
-      const ext = file.mimetype.split("/")[1];
+    storage: multer.diskStorage({
+        destination: function (req, file, cb) {
+            cb(null, "./upload-files");
+        },
+        filename: function (req, file, cb) {
+            const ext = file.mimetype.split("/")[1];
 
-      cb(null, file.fieldname + "-" + Date.now() + "." + ext);
-    }
-  })
+            cb(null, file.fieldname + "-" + Date.now() + "." + ext);
+        }
+    })
 };
 
-router.post("/", multer(multerConf).single("csvfile"), function(req, res) {
-  console.log(req.files);
-  res.send(req.files);
+
+// route for uploading
+router.post("/upload", multer(multerConf).single("csvfile"), function (req, res) {
+    console.log(req.files);
+    res.send(req.files);
+});
+
+
+router.get("/envoi-fichier", function (req, res) {
+    res.send("Page d'envoi de fichier");
 });
 
 module.exports = router;
