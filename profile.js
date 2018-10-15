58f6e51e488d2fa88575588cf9aac6e38392e94a
Tshili <Tshili@MacBook-Pro-de-Tshili.local>
2018-10-15 14:29:29 +0200

    csv to json

---
+++ profile.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var router = express.Router();
+var multer = require("multer");
+
+// where to store file
+const multerConf = {
+  storage: multer.diskStorage({
+    destination: function(req, file, cb) {
+      cb(null, "./upload-files");
+    },
+    filename: function(req, file, cb) {
+      const ext = file.mimetype.split("/")[1];
+
+      cb(null, file.fieldname + "-" + Date.now() + "." + ext);
+    }
+  })
+};
+
+router.post("/", multer(multerConf).single("csvfile"), function(req, res) {
+  console.log(req.files);
+  res.send(req.files);
+});
+
+module.exports = router;
