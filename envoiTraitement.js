2e331c0e80306c1ef13ed606c41276a26ea34ab4
Spokeek <spokeek@gmail.com>
2018-11-06 16:57:23 +0100

    update of the hash method of the first module

--- envoiTraitement.js
+++ envoiTraitement.js
@@ -2,18 +2,22 @@ var express = require('express')
 var router = express.Router()
 
 const fileFieldName = 'csvfile'
+const sha256 = require('js-sha256')
 
 router.post('/upload', function (req, res) {
   if (!(req.files || [])[fileFieldName]) {
     res.sendStatus(400)
   } else {
-    var startTime = new Date().getTime();
-    var elapsedTime = 0;
-    res.send(req.files[fileFieldName])
-
-    elapsedTime = new Date().getTime() - startTime;
-    console.log("====> execution time : " + elapsedTime + "ms");
-
+    var startTime = + new Date()
+    let data = req.files[fileFieldName].data.toString('utf8').split('\n')
+	data = data.map((line) => sha256(line))
+    var endTime = + new Date()
+	console.log((endTime-startTime)/1000 + " S")
+  res.render('envoi-traitement', {
+    fileFieldName,
+    modulePath: req.baseUrl,
+	data
+  })
   }
 })
 
