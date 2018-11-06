0250db5fe1faa46bc30eb8a9d6334f5e3cbdf739
Tshili <Tshili@MacBook-Pro-de-Tshili.local>
2018-11-06 15:47:54 +0100

    ajout du module image

--- envoiTraitement.js
+++ envoiTraitement.js
@@ -7,12 +7,21 @@ router.post('/upload', function (req, res) {
   if (!(req.files || [])[fileFieldName]) {
     res.sendStatus(400)
   } else {
+    var startTime = new Date().getTime();
+    var elapsedTime = 0;
     res.send(req.files[fileFieldName])
+
+    elapsedTime = new Date().getTime() - startTime;
+    console.log("====> execution time : " + elapsedTime + "ms");
+
   }
 })
 
 router.get('/', function (req, res) {
-  res.render('envoi-traitement', {fileFieldName, modulePath: req.baseUrl})
+  res.render('envoi-traitement', {
+    fileFieldName,
+    modulePath: req.baseUrl
+  })
 })
 
 module.exports = router
--- index.js
+++ index.js
@@ -11,16 +11,19 @@ app.set('view engine', 'ejs')
 app.use(fileUpload())
 
 const modulesToLoad = [
-	['envoi-traitement', "Envoi d'un fichier au traitement back", envoiTraitement],
-	['upload-list', "Upload d'une list de données, ensuite affichée dans un tableau", uploadListe],
+  ['envoi-traitement', "Envoi d'un fichier au traitement back", envoiTraitement],
+  ['upload-list', "Upload d'une list de données, ensuite affichée dans un tableau", uploadListe],
 ]
+//app.use(express.static('/Users/Tshili/Documents/Epsi/Green-IT/nodejs-version/pics'));
 
 const moduleRouterLoad = modulesToLoad.map((moduleArray) => {
-	app.use('/'+moduleArray[0], moduleArray[2])
-	
-	return [moduleArray[0], moduleArray[1]]
+  app.use('/' + moduleArray[0], moduleArray[2])
+
+  return [moduleArray[0], moduleArray[1]]
 })
-app.get('/', (req, res) => res.render('index', {moduleRouterLoad}))
+app.get('/', (req, res) => res.render('index', {
+  moduleRouterLoad
+}))
 
 app.listen(PORT, function () {
   console.log(`NodeJS Version Perf started on port ${PORT}!`)
